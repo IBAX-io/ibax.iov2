39ab927c5b18de787246d81379dd003cadff4adc
renlei <75403541+noahabel@users.noreply.github.com>
2021-11-08 18:06:03 +0800

    gtag

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -2,19 +2,29 @@
  * @Author: abc
  * @Date: 2020-12-18 09:49:47
  * @LastEditors: abc
- * @LastEditTime: 2020-12-30 18:58:08
+ * @LastEditTime: 2021-11-08 10:21:15
  * @Description:
  */
 export default ({ app: { router }, store }) => {
   if (process.client) {
-    console.log(window.dataLayer);
+    // console.log(location);
+    //  console.log(router);
+    //  console.log(document.title);
     window.dataLayer = window.dataLayer || [];
     // eslint-disable-next-line no-inner-declarations
     function gtag() {
       // eslint-disable-next-line no-undef
       dataLayer.push(arguments);
     }
-    gtag("js", new Date());
-    gtag("config", "G-3P74G75LD1");
+    gtag('js', new Date());
+    gtag('config', 'G-3P74G75LD1', {
+      page_title: document.title,
+      //  page_path: page,
+      page_location: location.href
+    });
+    router.afterEach((to, from) => {
+      gtag('set', 'page', to.fullPath);
+      gtag('send', 'pageview');
+    });
   }
 };
